365cc9d57753f4a56d82207d2be37e0085ac583b
n.see <gooarter@gmail.com>
2025-07-14 00:22:30 +0800

    添加 static serve && 修复tag中文编码

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -30,7 +30,7 @@ export const generateStaticParams = async () => {
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
   return tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
+    tag, // encodeURI(tag)
   }))
 }
 

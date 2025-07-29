80ab4dcf89a50f6955c2fe98d6aa7d091f8ae4b8
n.see <gooarter@gmail.com>
2025-07-29 23:15:36 +0800

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("722a3998e083be48")
+/******/ 		__webpack_require__.h = () => ("1244a53ec268bf4c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
